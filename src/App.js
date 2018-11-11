8ad842fee4ef46113a1660f90728f6d7d24c66bd
Remya Philipose <remyaphilipose@gmail.com>
2018-11-11 13:43:39 +1100

    react app initial cmmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -39,16 +39,9 @@ toggleHandler=()=>{
 }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-
-    };
+    
    let  persons = null;
+   let btnClass = '';
     if(this.state.flag){
       persons = (
         <div>
@@ -65,23 +58,23 @@ toggleHandler=()=>{
           }
          </div>
       );
-    style.backgroundColor='red';
     
+    btnClass = classes.red;
     }
-    const classes=[];
+    const assignedClasses=[];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi,I'm a React App</h1>
-        <p className = {classes.join(' ')}>This is really working </p>
+        <p className = {assignedClasses.join(' ')}>This is really working </p>
         <button 
+        className={btnClass}
         onClick={this.toggleHandler}
-        style = {style}
         >Switch Name</button>
         {persons}
         
